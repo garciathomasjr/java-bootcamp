eef9b4fb82167760e8bce6cb0a5c2ac099694dd9
Thomas Garcia, Jr <garcia.thomasjr@gmail.com>
2026-06-18 19:43:59 -0500

    Day 4 - methods and return values

---
+++ src/main/java/com/tgarcia/javabootcamp/MethodPractice.java
@@ -0,0 +1,18 @@
+package com.tgarcia.javabootcamp;
+
+public class MethodPractice {
+
+    public static void printFavoriteColor(String color) {
+        System.out.println("Your favorite color is " + color);
+    }
+
+    public static void main(String[] args) {
+
+        printFavoriteColor("Red");
+        printFavoriteColor("Blue");
+        printFavoriteColor("Green");
+
+    }
+}
+
+
